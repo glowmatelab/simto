17360efae941870b20745ce07d59e7e02f5a3b1c
Antavion <manishsamal832@gmail.com>
2026-06-25 22:59:26 +0530

    wetg

--- MusicBot/core/userbot.py
+++ MusicBot/core/userbot.py
@@ -2,13 +2,12 @@ from pyrogram import Client
 from config import Config
 
 
-class UserBot(Client):
-    def __init__(self):
-        super().__init__(
-            name="UserBot",
-            api_id=Config.API_ID,
-            api_hash=Config.API_HASH,
-            session_string=Config.STRING_SESSION,
-            sleep_threshold=30,
-            no_updates=False,
-        )
+def create_userbot():    # ✅ Original banana
+    return Client(
+        name="UserBot",
+        api_id=Config.API_ID,
+        api_hash=Config.API_HASH,
+        session_string=Config.STRING_SESSION,
+        sleep_threshold=30,
+        no_updates=False,
+    )
